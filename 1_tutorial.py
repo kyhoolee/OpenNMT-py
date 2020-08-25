6dd6bf7e3e1c7cf899e278b58ddbdd519557e0c9
kyhoolee <kyhoolee@gmail.com>
2020-08-25 08:57:47 +0700

    Update devices args

--- 1_tutorial.py
+++ 1_tutorial.py
@@ -12,16 +12,19 @@ def run(step=0):
         print('STEP-0: TRAIN MODEL')
         os.system('onmt_train -data data/tut1 -save_model tut1_model')
     elif step == 2:
-        print('STEP-0: PREPROCESS DATA')
-        os.system('onmt_translate -model demo-model_XYZ.pt -src data/src-test.txt -output pred.txt -replace_unk -verbose')
+        print('STEP-0: translate DATA')
+        os.system('onmt_translate -model tut1_model_XYZ.pt -src data/src-test.txt -output pred.txt -replace_unk -verbose')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--step', type=int, default=0, choices=[0, 1, 2],
                         help='specify which step to run')
+    parser.add_argument('--devices', type=str, default='3',
+                        help='specify which gpus to run')
 
     args = parser.parse_args()
     
+    os.environ["CUDA_VISIBLE_DEVICES"]=args.devices
     run(args.step)
 
